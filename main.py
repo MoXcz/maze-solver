48c31a09a5fc6c3cc2f7daf70e52429544775c62
mocos <130593875+MoXcz@users.noreply.github.com>
2025-01-24 20:40:12 -0500

    Add lines to be drawn on window frame

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from window import Window
+from window import Line, Point, Window
 
 
 def main():
     win = Window(800, 600)
+    point1 = Point(0, 100)
+    point2 = Point(200, 100)
+    line = Line(point1, point2)
+    win.draw_line(line, "white")
     win.wait_for_close()
 
 
--- window.py
+++ window.py
@@ -6,8 +6,8 @@ class Window:
         self.root = Tk()
         self.root.title("Maze Solver")
         self.root.protocol("WM_DELETE_WINDOW", self.close)
-        self.canva = Canvas(self.root, bg="black", height=height, width=width)
-        self.canva.pack(fill="both", expand=1)
+        self.canvas = Canvas(self.root, bg="black", height=height, width=width)
+        self.canvas.pack(fill="both", expand=1)
         self.on_window = False
 
     def redraw(self):
@@ -21,3 +21,23 @@ class Window:
 
     def close(self):
         self.on_window = False
+
+    def draw_line(self, line, fill_color="white"):
+        line.draw(self.canvas, fill_color)
+
+
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fill_color="white"):
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+        )
