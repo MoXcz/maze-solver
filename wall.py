6e74a01bde252fac6cb077da3e699ba85059f815
mocos <130593875+MoXcz@users.noreply.github.com>
2025-01-26 00:58:56 -0500

    ReRefactor Cell class to initialize in method
    Even if it makes more logical sense to have its initialization in the
    constructor, when working with Cells it results easier to define its
    dimensions only when it has to be drawn

--- wall.py
+++ wall.py
@@ -2,37 +2,30 @@ from window import Line, Point
 
 
 class Cell:
-    def __init__(
-        self,
-        win,
-        x1,
-        y1,
-        x2,
-        y2,
-        left=True,
-        right=True,
-        top=True,
-        bottom=True,
-    ):
-        self._x1 = x1
-        self._y1 = y1
-        self._x2 = x2
-        self._y2 = y2
+    def __init__(self, win, left=True, right=True, top=True, bottom=True):
         self._win = win
         self.has_left_wall = left
         self.has_right_wall = right
         self.has_top_wall = top
         self.has_bottom_wall = bottom
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
 
-    def draw(self):
+    def draw(self, x1, y1, x2, y2):
         """
         p1(x1,y2)  p2(x2, y2)
         p3(x1,y1)  p4(x2, y1)
         """
-        p1 = Point(self._x1, self._y2)
-        p2 = Point(self._x2, self._y2)
-        p3 = Point(self._x1, self._y1)
-        p4 = Point(self._x2, self._y1)
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        p1 = Point(x1, y2)
+        p2 = Point(x2, y2)
+        p3 = Point(x1, y1)
+        p4 = Point(x2, y1)
         if self.has_left_wall:
             l = Line(p1, p3)
             self._win.draw_line(l)
