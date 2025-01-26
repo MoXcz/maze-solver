8f04845302a7bfc3f06c694bf8c9ead31e7fd692
mocos <130593875+MoXcz@users.noreply.github.com>
2025-01-26 01:01:21 -0500

    Add frame drawing with a set column and row size
    The Maze class instantiates a maze by filling its inside with cells.

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+from maze import Maze
 from wall import Cell
 from window import Window
 
 
 def main():
-    win = Window(800, 600)
-    wall = Cell(win, 100, 100, 200, 200, right=False)
-    wall.draw()
-    wall2 = Cell(win, 300, 100, 400, 200, left=False)
-    wall2.draw()
-    wall.draw_move(wall2, True)
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
+
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
     win.wait_for_close()
 
 
